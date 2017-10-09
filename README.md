07d53d688277714cedbfec04d5073b73a799b9ef
David Shim <ssm0123@gmail.com>
2017-10-10 06:04:41 +0900

    done!

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
+This module supports conversion of the following.
+
+- Lists(to Sections with board view, exclude archived)
+- Cards(to Tasks, exclude archived)
+- Checklists(to SubTasks)
+- Comments(noted author name. asana doesn't support switch author.)
+- Assignee, Followers
+- Attachments
+
 # How to Use
 
-1. Export Your Trello Board Backup file as JSON
+1. Exports Your Trello Board Backup file as JSON
 
-2. Create Personal Token in Asana Connect
+2. Creates Trello Token
+- https://trello.com/app-key
+
+3. Creates Personal Token in Asana Connect
 - https://asana.com/guide/help/api/api#gl-connect
 
-3. Edit Configuration for matching members, select asana team, workspace and so on.
+4. Makes Configuration for matching members, select asana team, workspace and so on. You can easily use `-m` option for finding members. You should rename `config.json.sample` file to `config.json`.
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ var Promise = require("bluebird");
 var _ = require('underscore');
 var package = fs.readJsonSync('package.json');
 var Asana = require('asana');
+var Trello = require("node-trello");
+var request = require('request');
+var path = require('path');
 
 var LABEL_COLOR = {
     green: 'light-green',
@@ -77,9 +80,82 @@ var getUniqueName = function getUniqueName(name, haystack) {
     }
 };
 
+var convertMap = function convertToMap(data, map) {
+    if (_.isArray(data)) {
+        return _.compact(_.map(data, id => {
+            return convertToMap(id, map);
+        }));
+    }
+
+    if (typeof map[data] !== 'undefined') {
+        return map[data];
+    } else {
+        return null;
+    }
+};
+
+var fetchImage = function (url) {
+    return new Promise(function (resolve, reject) {
+        request.get({
+            url: url,
+            encoding: null
+        }, function (err, res, body) {
+            if (err || res.statusCode !== 200) {
+                reject(err || res.statusCode);
+                return;
+            }
+
+            if (body) {
+                resolve(body);
+            } else {
+                resolve(null);
+            }
+        });
+    });
+};
+
 fs.readJson(opts.config).then(function (config) {
     var client = Asana.Client.create().useAccessToken(config.asana.personal_access_token);
-    var projects = [];
+    var trello = new Trello(config.trello.key, config.trello.token);
+    var asanaData = {
+        projects: [],
+        tags: [],
+        users: []
+    };
+
+    var uploadImageToAsana = function (taskId, file, filename) {
+        return new Promise(function (resolve, reject) {
+            request.post({
+                url: `https://app.asana.com/api/1.0/tasks/${taskId}/attachments`,
+                headers: {
+                    Authorization: `Bearer ${config.asana.personal_access_token}`
+                },
+                formData: {
+                    file: {
+                        value: file,
+                        options: {
+                            filename: filename
+                        }
+                    }
+                }
+            }, function (err, res, body) {
+                if (err || res.statusCode !== 200) {
+                    reject(err || res.statusCode);
+                    return;
+                }
+
+                if (body) {
+                    try {
+                        body = JSON.parse(body);
+                    } catch (e) {}
+                }
+
+                resolve(body);
+            });
+        });
+    };
+
+    Promise.promisifyAll(trello);
 
     if (!config.asana.workspace) {
         console.log('You should select your workspace in asana.');
@@ -107,31 +183,37 @@ fs.readJson(opts.config).then(function (config) {
         });
     }
 
-    return client.projects.findByTeam(config.asana.team).then(fetch).then(results => {
-        projects = results;
-
+    // Prepare asana data to avoid duplicated
+    return Promise.join(
+        client.projects.findByTeam(config.asana.team).then(fetch),
+        client.tags.findByWorkspace(config.asana.workspace).then(fetch),
+        client.users.findByWorkspace(config.asana.workspace).then(fetch),
+        (projects, tags, users) => {
+            asanaData.projects = projects;
+            asanaData.tags = tags;
+            asanaData.users = users;
+        }
+    ).then(function () {
         return Promise.map(opts.files, parseJson);
     }).then(function (files) {
+        var trellMembers = _.flatten(_.pluck(files, 'members'));
+
         // Check only member list
         if (opts.onlyMembers) {
-            var members = _.flatten(_.pluck(files, 'members'));
-
             console.log('Trello Users');
-            console.log('<username>: <FullName>');
-            console.log(_.map(members, function (member) {
-                return `${member.username}: ${member.fullName}`;
+            console.log('<id>: <FullName>(<username>)');
+            console.log(_.map(trellMembers, function (member) {
+                return `${member.id}: ${member.fullName}(${member.username})`;
             }).join('\n'));
 
             console.log('\nAsana Users');
             console.log('<id>: <Name>');
 
-            return client.users.findByWorkspace(config.asana.workspace).then(fetch).then(users => {
-                _.each(users, user => {
-                    console.log(`${user.id}: ${user.name}`);
-                });
-
-                throw Promise.CancellationError;
+            _.each(asanaData.users, user => {
+                console.log(`${user.id}: ${user.name}`);
             });
+
+            throw Promise.CancellationError;
         }
 
         // Executes in order
@@ -140,15 +222,26 @@ fs.readJson(opts.config).then(function (config) {
             let listToSectionMap = {};
             let cardToTaskMap = {};
             let labelToTagMap = {};
+            let checklistMap = {};
+            let userMap = {};
+
+            _.each(file.checklists, checklist => {
+                checklistMap[checklist.id] = checklist;
+            });
+
+            _.each(asanaData.users, user => {
+                userMap[user.id] = user.name;
+            });
 
             // Creates a Project
             return client.projects.createInTeam(config.asana.team, {
-                name: getUniqueName(file.name, _.pluck(projects, 'name')),
+                name: getUniqueName(file.name, _.pluck(asanaData.projects, 'name')),
                 notes: file.desc,
                 layout: 'board'
             }).then(result => {
                 console.log(`Created ${result.name} project in your team.`);
                 projectData = result;
+                asanaData.projects.push(result);
 
                 // Creates sections in order
                 return Promise.mapSeries(file.lists, list => {
@@ -160,26 +253,124 @@ fs.readJson(opts.config).then(function (config) {
                     });
                 });
             }).then(() => {
-                // Creates tags
-                return Promise.map(file.labels, label => {
-                    return client.tags.create({
-
-                    }).then(result => {
-
+                // Filter exists tags same with label
+                var labels = _.filter(file.labels, label => {
+                    var matchedTag = _.find(asanaData.tags, tag => {
+                        return tag.name === label.name;
                     });
+
+                    if (matchedTag) {
+                        labelToTagMap[label.id] = matchedTag.id;
+                        return false;
+                    } else {
+                        return true;
+                    }
                 });
 
-                console.log(`Creating ${file.cards.length} parent tasks...`);
+                // Creates tags
+                console.log(`Creating ${labels.length} tags...`);
 
-                // Creates tasks
-                return Promise.map(file.cards, card => {
+                return Promise.map(labels, label => {
+                    return client.tags.createInWorkspace(config.asana.workspace, {
+                        name: label.name,
+                        color: LABEL_COLOR[label.color],
+                        notes: 'Created by Trello'
+                    }).then(result => {
+                        labelToTagMap[label.id] = result.id;
+                        asanaData.tags.push(result);
+                        console.log(`Created ${result.name}(${result.id}) tag.`);
+                    });
+                }, {
+                    concurrency: 3
+                }).then(function () {
+                    console.log(`Creating ${file.cards.length} tasks...`);
+                    let countTask = 0;
 
-                }, { concurrency: 3 });
+                    // Creates tasks
+                    return Promise.mapSeries(file.cards, card => {
+                        return client.tasks.create({
+                            assignee: card.idMembers.length ? convertMap(_.first(card.idMembers), config.member) : null,
+                            due_at: card.due,
+                            followers: card.idMembers.length > 1 ? convertMap(card.idMembers, config.member) : [],
+                            name: card.name,
+                            notes: card.desc,
+                            memberships: [{
+                                project: projectData.id,
+                                section: convertMap(card.idList, listToSectionMap)
+                            }],
+                            tags: card.idLabels.length ? convertMap(card.idLabels, labelToTagMap) : [],
+                            projects: [ projectData.id ]
+                        }).then(result => {
+                            var promises = [];
+                            var taskData = result;
+                            cardToTaskMap[card.id] = result.id;
+                            countTask++;
+
+                            if (countTask % 10 === 0) {
+                                console.log(`${countTask}...`);
+                            }
+
+                            if (card.idChecklists.length) {
+                                promises.push(
+                                    Promise.mapSeries(convertMap(card.idChecklists.reverse(), checklistMap), checklist => {
+                                        return Promise.mapSeries(checklist.checkItems.reverse(), item => {
+                                            return client.tasks.addSubtask(taskData.id, {
+                                                name: item.name,
+                                                completed: item.state !== 'incomplete'
+                                            });
+                                        }).then(function () {
+                                            return client.tasks.addSubtask(taskData.id, {
+                                                name: `${checklist.name}:`
+                                            });
+                                        });
+                                    })
+                                );
+                            }
+
+                            if (parseInt(card.badges.comments, 10) > 0) {
+                                promises.push(
+                                    // Trello export has limitation for count of actions as 1000. so we need to request directly trello API.
+                                    trello.getAsync(`/1/cards/${card.id}/actions?limit=1000`).then(result => {
+                                        var comments = _.filter(result, action => {
+                                            return action.type === 'commentCard';
+                                        });
+
+                                        return Promise.mapSeries(comments.reverse(), comment => {
+                                            var member = convertMap(comment.idMemberCreator, config.member);
+                                            var text = comment.data.text;
+                                            var memberName = member ? convertMap(member, userMap) : comment.memberCreator.fullName;
+
+                                            text = `${memberName}: ${text} from Trello`;
+
+                                            return client.tasks.addComment(taskData.id, {
+                                                text: text
+                                            });
+                                        });
+                                    })
+                                );
+                            }
+
+                            if (card.attachments.length) {
+                                promises.push(
+                                    Promise.mapSeries(card.attachments, attachment => {
+                                        return fetchImage(attachment.url).then(image => {
+                                            return uploadImageToAsana(taskData.id, image, path.basename(attachment.url));
+                                        });
+                                    })
+                                );
+                            }
+
+                            return Promise.all(promises);
+                        });
+                    });
+                });
+            }).then(function () {
+                console.log('complete!');
             });
         });
     });
+}).catch(reason => {
+    console.error(reason);
 }).catch(Promise.CancellationError, function (reason) {
     // nothing to do
-}).catch(function (reason) {
-    console.error(reason);
 });
